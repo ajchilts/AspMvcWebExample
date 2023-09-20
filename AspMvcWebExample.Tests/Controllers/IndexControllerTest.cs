430f25c2cc2c62aaf5dfbad8f59c1b022f5f3583
itfit <itfit@CHILTS>
2023-09-20 17:23:25 +0100

    Add project files.

---
+++ AspMvcWebExample.Tests/Controllers/IndexControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using AspMvcWebExample;
+using AspMvcWebExample.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace AspMvcWebExample.Tests.Controllers
+{
+    [TestClass]
+    public class IndexControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            IndexController controller = new IndexController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
