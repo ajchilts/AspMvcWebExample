c1e3f8e51251ac41b9860122f816a69eba7f91d6
Andy Chilton <40767309+ajchilts@users.noreply.github.com>
2023-09-22 14:36:00 +0100

    Added nspec testing class library and some basic tests

---
+++ testing-with-nspec/Controllers/ContactControllerSpec.cs
@@ -0,0 +1,22 @@
+﻿using NSpec;
+using FluentAssertions;
+using AspMvcWebExample.Controllers;
+using System.Web.Mvc;
+
+namespace testing_with_nspec
+{
+    public class ContactContollerSpec : nspec
+    {
+        void it_asserts_result_is_not_null()
+        {
+            // Arrange
+            ContactController controller = new ContactController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+    }
+}
